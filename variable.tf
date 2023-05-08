bd0e2d2d8d6eeca2e34b06d7fb447110ebde8e07
kdempire <128413009+Karandhawariya@users.noreply.github.com>
2023-05-08 15:58:13 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,122 @@
+variable "bucket1_name" {
+  type = any
+}
+
+variable "bucket2_name" {
+  type = any
+}
+
+variable "location" {
+  type = any
+}
+variable "object1_name" {
+  type = any
+}
+variable "object2_name" {
+  type = any
+}
+variable "object1_source" {
+  type = any
+}
+variable "object2_source" {
+  type = any
+}
+variable "function_name" {
+  type = any
+}
+variable "function2_name" {
+  type = any
+}
+variable "description" {
+  type = any
+}
+variable "region" {
+  type = any
+}
+variable "runtime" {
+  type = any
+}
+variable "event_type" {
+  type = any
+}
+variable "entry_point1" {
+  type = any
+}
+variable "entry_point2" {
+  type = any
+}
+variable "role1" {
+  type = any
+}
+variable "role2" {
+  type = any
+}
+variable "member1" {
+  type = any
+}
+variable "member2" {
+  type = any
+}
+variable "pub1_name" {
+  type = any
+}
+variable "pub2_name" {
+  type = any
+}
+variable "retention" {
+  type = any
+}
+variable "job1_name" {
+  type = any
+}
+variable "time_zone" {
+  type = any
+}
+variable "description_job1" {
+  type = any
+}
+variable "schedule1" {
+  type = any
+}
+variable "data1" {
+  type = any
+}
+variable "data2" {
+  type = any
+}
+variable "job2_name" {
+  type = any
+}
+variable "description_job2" {
+  type = any
+}
+variable "schedule2" {
+  type = any
+}
+
+variable "vm1_name" {
+  type = any
+}
+variable "vm2_name" {
+  type = any
+}
+variable "machine_type" {
+  type = any
+}
+variable "zone" {
+  type = any
+}
+variable "env_vm1" {
+  type = any
+}
+variable "env_vm2" {
+  type = any
+}
+variable "image" {
+  type = any
+}
+variable "network" {
+  type = any
+}
+
+
